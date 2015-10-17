8d9a90e4f21e7ec53bde7119aaf09a9bc5bec25d
Jack Somervell <jacksomervell@gmail.com>
2015-10-17 10:47:55 +0100

    initialised and set up basic app structure

---
+++ front-end/js/app.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var path = require('path');
+var cors = require('cors');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var app = express();
+
+var mongoose = require('mongoose');
